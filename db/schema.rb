d9059800e4c4650b562f2fbc96c66febb1101978
Michael TURCAN <michael13013@gmail.com>
2016-03-01 18:24:50 +0100

    add model product + like

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301150341) do
+ActiveRecord::Schema.define(version: 20160301172225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "size"
+    t.string   "price"
+    t.string   "color"
+    t.string   "product_type"
+    t.string   "brand"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
