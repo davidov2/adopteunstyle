ecf6f05837c279661fb43b08f9dc7000485578b3
Michael TURCAN <michael13013@gmail.com>
2016-03-01 18:34:52 +0100

    other models

---
+++ db/migrate/20160301173045_create_choices.rb
@@ -0,0 +1,10 @@
+class CreateChoices < ActiveRecord::Migration
+  def change
+    create_table :choices do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
