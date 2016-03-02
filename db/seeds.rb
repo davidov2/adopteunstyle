f5c4c4f42df2c1d43b4d403f534e8cb2838b98e3
david parienti <freshideababy@gmail.com>
2016-03-02 14:47:06 +0100

    seed product + view index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Seed product
-# Product.destroy_all
-
-# product1 = Product.create!(title: "jean", description: "taille basse", brand: "Levi's")
-# product2 = Product.create!(title: "chemise", description: "chemise blanche", brand: "Lacoste")
-
 # Seed User
-User.destroy_all
 user1 = User.create!(email: "admin@admin.com", password: "12345678")
 
 # Seed Look
-Look.destroy_all
 look1 = Look.create!(name: "Sport")
 
-
 # Seed Choice
-Choice.destroy_all
 choice1 = Choice.create!(user: user1, look: look1)
 
-# Seed Like
-# Like.destroy_all
-# like1 = Like.new
-# like1.product = product1
-# like1.user = user1
-# like1.save!
+# Seed Brand
+10.times do |n|
+  puts n
+  Brand.create!(name: 'Marque ' << n.to_s, description:'Description de la marque ' << n.to_s)
+end
 
-
+# Seed Product
+20.times do
+  Product.create!(title: Faker::Commerce.product_name)
+end
