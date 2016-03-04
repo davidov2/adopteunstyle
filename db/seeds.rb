6ba0899039a317865ee4e3ac0dae0406dbe65106
Matthieu Foillard <mgf@iotanet.net>
2016-03-04 15:39:10 +0100

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ supplier.each do |s|
       # Passe au suivant si l'ean n'est pas renseigné dans le flux
       next unless p.css('ean').first
       # Passe au suivante si c'est pas pour homme
-      next unless p.css('fields gender').first.content.strip == 'Men'
+      next unless p.css('fields img_large').first.try(:content).try(:strip) == 'Men'
       # On insere le produit que s'il n'existe pas (clé : ean)
       product = Product.where(ean: p.css('ean').first.content.strip).first
       if product
@@ -42,12 +42,11 @@ supplier.each do |s|
         # Bon le search avec css c'est crado je pense mais j'ai pas le temps ...
         product = Product.new
         product.title = p.css('name').first.content.gsub(/\n/, '').strip
-        product.description = p.css('description').first.content.strip
         product.image = p.css('imageurl').first.content.strip unless p.css('imageurl').first.nil?
         product.link = p.css('advertiserproducturl').first.content.strip
+        product.description = p.css('description').first.content.strip
         product.ean = p.css('ean').first.content.strip
         product.size = p.css('size').first.content.strip
-        product.price = p.css('price').first.content.strip
         product.color = p.css('fields colour').first.content.strip
         product.brand = Brand.first_or_create!(name: p.css('brand').first.content.strip)
         if category = p.css('TDCategoryID').first
