e34e047e058df79d0c0088cbfc7112e379dbe83a
Matthieu Foillard <mgf@iotanet.net>
2016-03-08 17:17:33 +0100

    Search en GET et mise en session des choix de look

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -1,6 +1,8 @@
 class LooksController < ApplicationController
 
   def index
+    search_params = session['search_params']
+    @looks_selected = search_params['looks'].reject{|k,v| v == "0"}.keys if search_params
     @looks = Look.all
   end
 
