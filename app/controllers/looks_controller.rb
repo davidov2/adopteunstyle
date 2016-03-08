ed163a1440f2c5c0f0424616528689c7847aaa09
Matthieu Foillard <mgf@iotanet.net>
2016-03-08 17:30:42 +0100

    Fix

--- app/controllers/looks_controller.rb
+++ app/controllers/looks_controller.rb
@@ -2,6 +2,7 @@ class LooksController < ApplicationController
 
   def index
     search_params = session['search_params']
+    @looks_selected = []
     @looks_selected = search_params['looks'].reject{|k,v| v == "0"}.keys if search_params
     @looks = Look.all
   end
